f42f354bdb4f6edcb6e80cc3f0d7eeccd5e1e69d
Shanmukha Anusree Ukkalam <127738296+anusreeukkalam@users.noreply.github.com>
2024-10-02 11:19:37 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,9 +7,13 @@ export default function LoginPage(){
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
     const [redirect,setRedirect]=useState(false);
+    const [isSubmitting,setIssubmitting]=useState(false);
     const {setUser}=useContext(UserContext);
+    
     async function handleLoginSubmit(ev){
     ev.preventDefault();
+        if(isSubmitting) return;
+        setIsSubmitting(true);
     try{
     const {data}= await axios.post('/login',{email,password},{withCredentials:true});
     setUser(data);
@@ -18,7 +22,15 @@ export default function LoginPage(){
     }
     catch(e)
     {
-       alert('login failed')
+        if(e.response && e.response.status===400){
+       alert('login failed: invalid credentials or user not registered')
+        }
+        else{
+            alert('login failed: something went wrong')
+        }
+        setEmail('');
+        setPassword('');
+        setIsSubmitting(false);
     }
 }
 
@@ -34,12 +46,16 @@ if(redirect){
             <input type="email" 
                    placeholder="your@email.com" 
                    value={email} 
-                   onChange={ev=>setEmail(ev.target.value)}/>
+                   onChange={ev=>setEmail(ev.target.value)}
+                   diasbled={isSubmitting}/>
             <input type="password" 
                    placeholder="type your password" 
                    value={password} 
-                   onChange={ev=>setPassword(ev.target.value)}/>
-            <button className="primary">Sign in</button>
+                   onChange={ev=>setPassword(ev.target.value)}
+                   diasbled={isSubmitting}/>
+            <button className="primary">
+                {isSubmitting ? 'Signing in...' : 'Sign in'}
+            </button>
             <div className="text-center py-2 text-gray-500">
                 Don't have an account to login? <Link className="underline text-black " to={'/register'}>Sign Up here!</Link>
             </div>
